5e67d82eca1180f986e88d300e52b242c14dd5e8
gysobu <gysobu@gmail.com>
2019-01-25 14:57:33 -0600

    added routes and views

--- setupbeatburner.sql
+++ setupbeatburner.sql
@@ -1,31 +1,36 @@
 
 -- DROP TABLE users,difficulty,music,register;
 
-CREATE TABLE users(
-    id SERIAL PRIMARY KEY,
-    firstName VARCHAR(20),
-    lastName VARCHAR(20),
-    gender VARCHAR(2),
-    weight REAL,
-    age INTEGER
-);
+-- DROP TABLE users;
+-- DROP TABLE music;
+-- CREATE TABLE users(
+--     id SERIAL PRIMARY KEY,
+--     firstName VARCHAR(20),
+--     lastName VARCHAR(20),
+--     gender VARCHAR(2),
+--     weight REAL,
+--     age INTEGER
+-- );
 
-CREATE TABLE difficulty(
-id SERIAL PRIMARY KEY,
-difficulty VARCHAR(20)
-);
+-- CREATE TABLE difficulty(
+-- id SERIAL PRIMARY KEY,
+-- difficulty VARCHAR(20)
+-- );
 
 CREATE TABLE music(
     id SERIAL PRIMARY KEY,
     difficultyID INTEGER REFERENCES difficulty(id),
-    title VARCHAR(20),
+    title VARCHAR(90),
     bpm INTEGER,
     artistName VARCHAR(20),
-    calories REAL
+    calories REAL,
+    duration NUMERIC
 );
 
-CREATE TABLE register(
-    sid INTEGER,
-    userName VARCHAR(20),
-    passcode VARCHAR
-);
+-- CREATE TABLE register(
+--     sid INTEGER,
+--     userName VARCHAR(20),
+--     passcode VARCHAR
+-- );
+
+ COPY music(id,difficultyID,title,bpm,artistName,calories,duration) FROM '/Users/sobha/BeatBurner/public/music.csv' DELIMITER ',' CSV HEADER;
