ebe9e116b003d37e8eadcaf4ebf668214c7b551a
John <argote.john@gmail.com>
2019-03-14 22:08:57 -0500

    updated

--- routes/hard.js
+++ routes/hard.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var dbs = require('../models/modelsql/database.js')
 
+//get the song list for hard mode from database
 router.get('/hard', (req, res) => {
     dbs.any("select music.* from music inner join difficulty on music.difficultyid=difficulty.id and difficulty.difficulty='hard'")
         .then((results) => {
